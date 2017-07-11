3c2ea598336514b52ffdd3ed461dafe78ca34072
wangyasen <wangyasen@jd.com>
2017-07-11 18:59:05 +0800

    add export

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import ReactAlloyTouch from './ReactAlloyTouch';
 import reactPull from './ReactPull';
 import reactCarousel from './ReactCarousel';
+import reactSwiper from './ReactSwiper';
 
 export const ReactPull = reactPull;
 export const ReactCarousel = reactCarousel;
+export const ReactSwiper = reactSwiper;
 
 export default ReactAlloyTouch;
