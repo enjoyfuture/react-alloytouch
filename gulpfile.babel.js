4dbac42727b2da190c81e36238c8b743ee046d77
wangyanjun <linder0209@126.com>
2017-09-15 23:51:24 +0800

    init branch gh-pages

--- gulpfile.babel.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import requireDir from 'require-dir';
-
-requireDir('./gulp');
-
-// 默认任务
-gulp.task('default', () => {
-  gulp.start('build');
-});
--- js/AlloyTouch.js
+++
@@ -1,8 +0,0 @@
-import AlloyTouch from 'alloytouch';
-
-// 重新设置 AlloyTouch 方法
-AlloyTouch.prototype.setOption = function (key, value) {
-  this[key] = value;
-};
-
-export default AlloyTouch;
